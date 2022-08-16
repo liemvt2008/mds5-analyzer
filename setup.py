48c822ddd1c4def8c4aa02f7ead6136ee720ec6d
liem <liemvt2008@gmail.com>
2022-08-16 15:17:47 +0700

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ttth-mds5-analyzer',
-    version='0.0.1',
+    version='0.0.3',
     license='MIT',
     author="Data Farmer",
     author_email='datafarmer2019@gmail.com',
