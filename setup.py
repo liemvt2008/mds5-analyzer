fb1cd45e98fbd4ec83272b09f2dba0eac05f0480
admin <liemvt2008>
2022-10-29 16:43:10 +0700

    update new feature

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ttth-mds5-analyzer',
-    version='0.0.8',
+    version='0.0.8.8',
     license='MIT',
     author="Data Farmer",
     author_email='datafarmer2019@gmail.com',
