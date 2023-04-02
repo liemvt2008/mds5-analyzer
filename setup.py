7dc191757ceb395de78b86d93cdd5ae2658fe18f
admin <liemvt2008>
2023-04-03 00:35:23 +0700

    add data to package

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='ttth-mds5-analyzer',
-    version='0.1.2',
+    version='0.1.7',
     license='MIT',
     author="Data Farmer",
     author_email='datafarmer2019@gmail.com',
     packages=find_packages('analyzer'),
+    include_package_data=True,
     package_dir={'': 'analyzer'},
+    package_data={'': ['meta_data/*.txt']},
     url='https://github.com/liemvt2008/mds5-analyzer',
     keywords='mds5-analyzer',
     install_requires=[
@@ -16,7 +18,11 @@ setup(
             'pandas',
             'seaborn',
             'scipy',
-            'statsmodels'
+            'statsmodels',
+            'scipy',
+            'regex',
+            'underthesea',
+            'setuptools'
       ],
 
 )
